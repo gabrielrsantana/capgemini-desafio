1f810fa8e5e4d38b84632853606a341d7ce3d0d1
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2022-03-06 16:08:30 -0300

    Questão 03 Desafio Capgemini
    comentando linhas desnecessárias

--- Encriptacao.java
+++ Encriptacao.java
@@ -1,64 +1,85 @@
+/*
+Desafio Capgemini  Questão 03
+ */
 package com.mycompany.projeto1;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * questão 03 - Desafio Capgemini
+ 
  * @author Gabriel Rocha Santana 06/03/2022
  */
 public class Encriptacao {
     
     public static void main(String[] args) {
+    // declarando as variáveis    
     int i=0,lin,col=0,indice=0; //linha e coluna para o laço For
-    double colunas,linhas=0; //calculo das linhas com raiz quadrada,exibe double
+    double colunas,linhas=0; //calculo das linhas com raiz quadrada,exige double
     String texto="",textoOriginal="";
-    char [][] matriz = new char[4][4]; //matriz com os  caracteres da string do input
+    char [][] matriz; //matriz para o grid dos elementos do texto
     
+    //input do usuario 
     Scanner sc = new Scanner(System.in);
     System.out.println("Digite o texto para ser encriptado:");
-    //remove os espacos internos
     textoOriginal=sc.nextLine();
+    //remove os espacos externos e internos
     texto = textoOriginal.trim().replace(" ","");
-    System.out.println("texto é="+texto);
-    System.out.println("tamanho é="+texto.length()+" caracteres");
+   //para ver texto depois de removido os espacos,descomente as 02 linhas abaixo 
+   // System.out.println("texto ficou ="+texto);
+   // System.out.println("O texto tem = "+texto.length()+" caracteres");
     
-    colunas=Math.ceil(Math.sqrt((texto.length()-1))); //total de colunas
+    //calcular o total de colunas e linhas com raiz quadrada
+    colunas=Math.ceil(Math.sqrt((texto.length()-1)));
     linhas=colunas; //total de linhas
-    System.out.println("\n Colunas="+ (int)colunas);
-    System.out.println("\n Linhas="+ (int)linhas);
-     System.out.println("preenchendo matriz grid...\n");
-   //
+    //instancia matriz com tamanho total de linhas/colunas 
+    matriz = new char[(int)linhas][(int)colunas];
+    boolean res= validacaoTamanho((int)linhas,(int)colunas,texto.length());
+    if(res){
+        System.out.println("tamanhos validos..encriptando...\n");
+        
+        
+    }else{
+        System.out.println("Teste de validação falhou.Saindo do programa...");
+        System.exit(0);
+    }
+   //preenchendo matriz grid com os caracteres do texto digitado...
     for (lin = 0; lin < linhas;lin++) {
         for (col = 0; col < colunas;col++) {
             if(indice < texto.length()){
-                matriz[lin][col]=texto.charAt(indice) ;
-              //  System.out.println("indice= "+indice+" - "+matriz[lin][col]); //salvou o caracter na matrix..incremente depois
+                matriz[lin][col]=texto.charAt(indice);
                 indice++;
             }
-            
         }//colunas	
     }//linhas
     
-   System.out.println("String antes: :"+textoOriginal); 
-   System.out.println("String encriptada:"); 
-       for (col = 0; col < colunas;col++) {
+   System.out.println("Texto antes: :"+textoOriginal); 
+   System.out.println("Texto encriptado:"); 
+   for (col = 0; col < colunas;col++) {
         for (lin = 0; lin < linhas;lin++) {
             if((lin %linhas) == 0){
                System.out.print(" ");
             }
           System.out.print(matriz[lin][col]); 
             
-        }//colunas	
-    }//linhas
+        }//linhas	
+    }//colunas
+ 
    
-      
-
-    
+   
+   
     }//main
     
-public static String addChar(String str, char ch, int position) {
-    return str.substring(0, position) + ch + str.substring(position);
+
+
+//testa condição linhas x colunas >= Tam texto
+public static boolean validacaoTamanho(int lin,int col,int tamTexto){
+    System.out.println("tamanho do texto="+tamTexto);
+    if(((lin * col) >= tamTexto)){
+        return true;
+    }else { 
+        return false;
+    }
+    
 }
     
 }//classe encriptar
